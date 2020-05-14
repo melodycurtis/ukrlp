7652f9cd3040f30361e06c78fbad733be8d64e5c
melodycurtis <melodycurtis123@gmail.com>
2020-05-14 12:53:51 +0100

    start

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Service name goes here',
+  serviceName: 'UK Register of Learning Providers',
 
   // Default port that prototype runs on
   port: '3000',
